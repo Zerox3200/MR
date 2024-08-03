00b7932b0910b6af9f67ed45a6eda173369966aa
ahmedmohame19 <96324257+ahmedmohame19@users.noreply.github.com>
2024-08-03 19:36:15 +0300

    edits

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -46,7 +46,7 @@ export default function Order() {
                 </div>
 
                 <div>
-                    <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">العنوان</label>
+                    <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">العنوان بالتفصيل</label>
                     <motion.Textarea dir='rtl' type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                      focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="اكتب عنوانك هنا بالتفصيل مع ذكر المحافظة - اسم المنطقة - اسم الشارع - رقم العمارة - علامة مميزة إن وُجِد" required
                         initial={{ opacity: 0, y: 10 }} animate={InView && { opacity: 1, y: 0 }}
